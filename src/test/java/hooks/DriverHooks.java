33f35326b58cceb5f7a2fb5220871e656a036e08
TarasYanUA <wildmonach@gmail.com>
2024-04-19 17:56:56 +0300

    Начал работу над мобильной версией тест-кейсов.

--- src/test/java/hooks/DriverHooks.java
+++ src/test/java/hooks/DriverHooks.java
@@ -4,15 +4,21 @@ import com.codeborne.selenide.*;
 import io.cucumber.java.After;
 import io.cucumber.java.Before;
 import org.assertj.core.api.SoftAssertions;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.*;
 
 public class DriverHooks {
-    public static final String BASIC_URL = "https://trs.test.abt.team/4172mvru/admin.php?dispatch=addons.manage";
+    public static final String BASIC_URL = "https://trs.test.abt.team/4172ultru/admin.php?dispatch=addons.manage";
 
     public DriverHooks() {super();}
 
-    @Before()
+/*    @Before()
     public void openBrowser() {
         Configuration.browser = "chrome";
         open(BASIC_URL);
@@ -26,7 +32,26 @@ public class DriverHooks {
 
         $(".btn.btn-primary").click();
         $("#bp_off_bottom_panel").click();
+    }*/
+
+    @Before() //под мобильное устройство
+    public void prepareBrowser() {
+        Map<String, String> mobileEmulation = new HashMap<>();
+        mobileEmulation.put("deviceName", "Nexus 5");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.setExperimentalOption("mobileEmulation", mobileEmulation);
+        WebDriver driver = new ChromeDriver(chromeOptions);
+        WebDriverRunner.setWebDriver(driver);   // Устанавливаем созданный драйвер как текущий драйвер для Selenide
+
+        open(BASIC_URL);
+        Configuration.timeout = 4000; //Общая задержка
+        Configuration.holdBrowserOpen = false; //не закрываем браузер пока ведём разработку
+        Configuration.screenshots = true; //делаем скриншоты при падении
+
+        $(".btn.btn-primary").click();
+        $("#bp_off_bottom_panel").click();
     }
+
     @After
     public void closerBrowser() {
         SoftAssertions softAssertions = CollectAssertMessages.getSoftAssertions();
